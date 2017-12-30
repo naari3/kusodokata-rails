f89b2a4317084ce90cef5c089548f59337b69ea5
Yuuki Sone <yuki_sone@dwango.co.jp>
2017-12-31 03:15:39 +0900

    add autoload to lib

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,8 @@ module KusodokataRails
       generator.assets false
       generator.template_engine :slim
     end
+
+    config.autoload_paths << Rails.root.join('lib')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
