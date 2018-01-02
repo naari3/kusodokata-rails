11eb2525a9d3b84e8c7cf7d755ef50e6a9db23b0
Yuuki Sone <yuki_sone@dwango.co.jp>
2018-01-03 01:50:22 +0900

    use history API

--- app/frontends/application.js
+++ app/frontends/application.js
@@ -1,1 +1,21 @@
 // any codes here...
+import 'babel-polyfill';
+require('classlist-polyfill');
+import domready from 'domready';
+import { h, app } from "hyperapp"
+
+// const state = {}
+// const actions = {}
+//
+// const view = (state, actions) =>
+//   h("main", {}, [
+//     h("h1", {}, state.count),
+//     h("button", { onclick: () => actions.down(1) }, "–"),
+//     h("button", { onclick: () => actions.up(1) }, "+")
+//   ])
+//
+// domready(() => {
+//   const main = app(state, actions, view, document.body)
+// })
+
+history.replaceState('','','/')
