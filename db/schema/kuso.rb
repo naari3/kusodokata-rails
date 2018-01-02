28db9411fffc9d0cab7cd4abf98579bb7af0f60a
Yuuki Sone <yuki_sone@dwango.co.jp>
2018-01-03 03:21:32 +0900

    add unique unique_id

--- db/schema/kuso.rb
+++ db/schema/kuso.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 create_table :kusos, force: :cascade, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4' do |t|
-  t.string :unique_id, index: true
+  t.string :unique_id, index: true, unique: true
   t.text :body
 
   t.datetime :created_at, null: false
