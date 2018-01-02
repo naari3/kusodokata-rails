6697cc2dca1de56160d3e3ed0e06bf8ed6f90ca4
Yuuki Sone <yuki_sone@dwango.co.jp>
2018-01-03 02:43:25 +0900

    set device_variant

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_request_variant
+
+  private
+
+  def set_request_variant
+    request.variant = request.device_variant
+  end
 end
