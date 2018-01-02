c753f4888df2010226e64ce4d51b9ee9301799b2
Yuuki Sone <yuki_sone@dwango.co.jp>
2018-01-03 03:06:34 +0900

    add index

--- db/schema/kuso.rb
+++ db/schema/kuso.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 create_table :kusos, force: :cascade, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4' do |t|
-  t.string :unique_id
+  t.string :unique_id, index: true
   t.text :body
 
   t.datetime :created_at, null: false
