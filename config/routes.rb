a9eab457120edb311695d4cf636c43cf63e925cd
Yuuki Sone <yuki_sone@dwango.co.jp>
2018-01-06 02:28:27 +0900

    add new

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :kusos, param: :unique_id, only: %i[show]
+      resources :kusos, param: :unique_id, only: %i[new show]
     end
   end
 end
