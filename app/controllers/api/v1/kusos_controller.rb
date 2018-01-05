353a06ca9d042642a4fd14f65a584e0bd9b0d6ca
Yuuki Sone <yuki_sone@dwango.co.jp>
2018-01-05 20:24:42 +0900

    implement kuso api

--- app/controllers/api/v1/kusos_controller.rb
+++ app/controllers/api/v1/kusos_controller.rb
@@ -1,4 +1,10 @@
 class Api::V1::KusosController < ApplicationController
+  def new
+    @kuso = Kuso.new
+    @kuso.save
+  end
+
   def show
+    @kuso = Kuso.find_by(unique_id: params[:unique_id])
   end
 end
