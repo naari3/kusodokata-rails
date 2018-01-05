f939119d68f8339087de3c84af2d26506240f82e
Yuuki Sone <yuki_sone@dwango.co.jp>
2018-01-05 19:58:38 +0900

    add routes for api/v1/kusos

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   get '/', to: 'kusos#new'
   resources :kusos, param: :unique_id, only: %i[show], path: '/'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :kusos, only: %i[show]
+    end
+  end
 end
