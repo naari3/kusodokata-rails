9906d20a05e51a19781019df37f3695e2733b117
Yuuki Sone <yuki_sone@dwango.co.jp>
2018-01-05 18:27:37 +0900

    comfort rule for me

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,36 +2,32 @@ module.exports = {
   "env": {
     "browser": true,
     "commonjs": true,
-    "es6": true,
     "node": true
   },
 
-  "extends": "eslint:recommended",
+  "ecmaFeatures": {
+    "jsx": true
+  },
+
+  "extends": "standard",
 
   "parserOptions": {
     "sourceType": "module"
   },
 
   "rules": {
-    "indent": [
-      "error",
-      2
-    ],
-
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
 
     "quotes": [
       "error",
       "single"
     ],
 
-    "semi": [
-      "error",
-      "always"
+    "no-unused-vars": [
+      "warn",
+      {
+        "vars" : "all",
+        "args" : "after-used"
+      }
     ]
-
   }
 };
