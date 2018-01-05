8215bcba02ceaeeda2f2448a5c02cf4696c704f1
Yuuki Sone <yuki_sone@dwango.co.jp>
2018-01-05 19:45:17 +0900

    rubocop -a

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Rack::Attack
-  throttle('req/ip', :limit => 300, :period => 5.minutes) { |req| req.ip}
+  throttle('req/ip', limit: 300, period: 5.minutes, &:ip)
 end
