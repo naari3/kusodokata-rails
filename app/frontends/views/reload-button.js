283e96a12f62ccdb0ceb3aa8c27be3bac87c8138
Yuuki Sone <yuki_sone@dwango.co.jp>
2018-01-06 15:24:24 +0900

    add outlined style

--- app/frontends/views/reload-button.js
+++ app/frontends/views/reload-button.js
@@ -2,7 +2,7 @@ import {h} from 'hyperapp'
 
 export const ReloadButton = ({state, actions}) =>
   <button
-    class={'button is-accent ' + (state.isLoading ? 'is-disabled' : '')}
+    class={'button is-accent is-outlined ' + (state.isLoading ? 'is-disabled' : '')}
     onclick={actions.reload}
     disabled={state.isLoading}>もう一度やりたいぜ。
   </button>
