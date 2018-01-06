80a426aa02b9b100dc5bf2db29e1fdb74cc82cc1
Yuuki Sone <yuki_sone@dwango.co.jp>
2018-01-07 00:43:11 +0900

    update meta_tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,18 @@ module ApplicationHelper
       site: t('meta.site.name'),
       separator: t('meta.site.separator', default: '-'),
       canonical: canonical_href,
-      reverse: true
+      viewport: 'initial-scale=1',
+      description: '変態糞土方による並外れた文才によって生み出された名文から新しい文章を生み出すアプリ',
+      reverse: true,
+      og: {
+        title: :title,
+        type: 'website',
+        url: request.original_url,
+        site_name: t('meta.site.name'),
+        image: image_url('620.png'),
+        description: :description,
+        locale: 'ja_JP'
+      }
     }
   end
 end
