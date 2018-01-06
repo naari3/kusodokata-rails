0e7d9fadf0ae1d1c9c3a30b961f6594164cba085
Yuuki Sone <yuki_sone@dwango.co.jp>
2018-01-06 15:25:49 +0900

    change reloading messages

--- app/frontends/views/reload-button.js
+++ app/frontends/views/reload-button.js
@@ -4,5 +4,5 @@ export const ReloadButton = ({state, actions}) =>
   <button
     class={'button is-accent is-outlined ' + (state.isLoading ? 'is-disabled' : '')}
     onclick={actions.reload}
-    disabled={state.isLoading}>もう一度やりたいぜ。
+    disabled={state.isLoading}>{state.isLoading ? 'ああ＾～' : 'もう一度やりたいぜ。'}
   </button>
