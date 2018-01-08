94cf964128c33498146f4b2beefd70aca50fc477
Yuuki Sone <yuki_sone@dwango.co.jp>
2018-01-08 22:32:14 +0900

    delete official twitter share button

--- app/frontends/actions/index.js
+++ app/frontends/actions/index.js
@@ -1,12 +1,9 @@
 import { reload } from './reload'
 import { setKuso, setLoading } from './sets'
 import { updateShareTwitterUrl } from './update_share_twitter_url'
-import { updateShareTwitterButton } from './update_share_twitter_button'
 
 export const actions = {
   reload: e => async (state, actions) => reload(state, actions),
   setKuso: kuso => setKuso(kuso),
-  setLoading: isLoading => setLoading(isLoading),
-  updateShareTwitterUrl: () => state => updateShareTwitterUrl(state),
-  updateShareTwitterButton: () => updateShareTwitterButton()
+  setLoading: isLoading => setLoading(isLoading)
 }
