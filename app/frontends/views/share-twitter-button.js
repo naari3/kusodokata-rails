5edd57454cbe8fe075b1ac50e8eb0283855a5522
Yuuki Sone <yuki_sone@dwango.co.jp>
2018-01-08 22:32:45 +0900

    implement unofficial twitter button

--- app/frontends/views/share-twitter-button.js
+++ app/frontends/views/share-twitter-button.js
@@ -2,6 +2,8 @@ import {h} from 'hyperapp'
 
 export const ShareTwitterButton = ({state}) =>
   <a
-    class='_twitter-share-button'
+    class='button share twitter'
+    target='_blank'
     href={state.shareTwitterUrl}>
+    <i class='icon twitter'></i>
   </a>
