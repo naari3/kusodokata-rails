b5ab569172232fbc6cb2b26570cd521e93220ac9
Yuuki Sone <yuki_sone@dwango.co.jp>
2018-01-14 08:08:12 +0900

    add noopener by google advice

--- app/frontends/views/share-twitter-button.js
+++ app/frontends/views/share-twitter-button.js
@@ -4,6 +4,7 @@ export const ShareTwitterButton = ({state}) =>
   <a
     class='button share twitter'
     target='_blank'
+    rel='noopener'
     href={state.shareTwitterUrl}>
     <i class='icon twitter'></i>
   </a>
